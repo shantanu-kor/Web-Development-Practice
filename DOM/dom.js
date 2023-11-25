b0659c1bc4862d0dccd54e3860b25a38ffd2f624
Shantanu Kor <77278554+shantanu-kor@users.noreply.github.com>
2023-11-25 17:28:10 +0530

    querySelector and querySelectorAll

--- DOM/dom.js
+++ DOM/dom.js
@@ -62,4 +62,40 @@ for (let i = 0; i < li.length; i++){
     li[i].style.backgroundColor = "#f4f4f4";
 }
 
-// 
+// QUERYSELECTOR //
+// let header = document.querySelector('#main-header');
+// header.style.borderBottom = 'solid 4px #ccc';
+
+// let input = document.querySelector('input'); // selects the first //
+// input.value = 'Hello World';
+
+// let submit = document.querySelector('input[type="submit"]');
+// submit.value = "SEND";
+
+// let item = document.querySelector('.list-group-item');
+// item.style.color = 'red';
+
+// let lastItem = document.querySelector('.list-group-item:last-child');
+// lastItem.style.color = 'blue';
+
+let secondItem = document.querySelector('.list-group-item:nth-child(2)');
+secondItem.style.backgroundColor = 'green';
+
+let thirdItem = document.querySelector('.list-group-item:nth-child(3)');
+thirdItem.style.visibility = 'hidden';
+
+
+// QUERYSELECTORALL //
+// let titles = document.querySelectorAll('.title');
+
+// console.log(titles);
+// titles[0].textContent = 'Hello';
+
+let odd = document.querySelectorAll('li:nth-child(odd)');
+
+for (let i = 0; i < odd.length; i++){
+    odd[i].style.backgroundColor = 'green';
+}
+
+let all = document.querySelectorAll('li');
+all[1].style.color = 'green';
